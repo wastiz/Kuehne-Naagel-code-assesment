c8d7cea2e3e3281fa0cf0ac2cc32b361b4f89223
Wastiz <valnos04@gmail.com>
2022-10-01 18:56:57 +0300

    another

--- src/components/shipment-add-form/ShipmentAddForm.js
+++ src/components/shipment-add-form/ShipmentAddForm.js
@@ -5,9 +5,31 @@ const ShipmentAddForm = () => {
         <div className="add-form">
             <hr/>
             <h4>Shipment details</h4>
-            <form>
-                <label>orderNo</label>
-                <input type="text" placeholder=""/>
+            <form className='add-form-form'>
+                <div className="add-form-item">
+                    <label>orderNo</label>
+                    <input type="text" placeholder="Enter order number"/>
+                </div>
+                <div className="add-form-item">
+                    <label>Date</label>
+                    <input type="text" placeholder="Enter order date"/>
+                </div>
+                <div className="add-form-item">
+                    <label>Customer</label>
+                    <input type="text" placeholder="Enter order customer"/>
+                </div>
+                <div className="add-form-item">
+                    <label>TrackingNo</label>
+                    <input type="text" placeholder="Enter order tracking number"/>
+                </div>
+                <div className="add-form-item">
+                    <label>Consignee</label>
+                    <input type="text" placeholder="Enter order consignee"/>
+                </div>
+                <div className="add-form-item">
+                    <label>Status</label>
+                    <input type="text" placeholder="Enter order status"/>
+                </div>
             </form>
             <hr/>
         </div>
